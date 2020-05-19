f35bc6a5c8d70a30c35afb240e0b616bb63b90e9
Bryan J Wong <bryanjwong@g.ucla.edu>
2020-05-19 12:44:47 -0700

    testing button init

--- Project4B/lab4b.c
+++ Project4B/lab4b.c
@@ -10,8 +10,23 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <errno.h>
+#include <mraa/gpio.h>
+
+#define TEMP_MRAA   1
+#define BUTTON_MRAA 60
+
+void handle_button_press() {
+  // output and log final sample with time and string SHUTDOWN instead of temp
+  mraa_gpio_close(button);
+  exit(0);
+}
 
 int main(int argc, char *argv[]) {
+  /* Configure MRAA GPIO Input Button */
+  mraa_gpio_context button = mraa_gpio_init(BUTTON_MRAA);
+  mraa_gpio_dir(button, MRAA_GPIO_IN);
+  mraa_gpio_isr(button, MRAA_GPIO_EDGE_RISING, &handle_button_press, NULL);
+
   /* Parse possible --threads and --iterations options */
   struct option long_options[] = {
     {"period",      required_argument,  0,  'p'}, // Sampling interval in seconds
@@ -29,3 +44,8 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
   }
+
+
+  mraa_gpio_close(button);
+  return 0;
+}
